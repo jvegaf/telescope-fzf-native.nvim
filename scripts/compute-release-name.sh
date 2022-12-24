08c83be012aadb5a3e4d58312d3a9dbfa71afba2
Zeno Jiricek <airtonix@users.noreply.github.com>
2022-12-25 09:26:05 +1030

    initial attempt to automate releases

---
+++ scripts/compute-release-name.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+MAINLINE_BRANCH="main"
+RELEASE_BRANCH_PATTERN="^releases/(.*)"
+REF="${1}"
+COMMITHASH="${2}"
+
+
+if [ "${REF}" -eq "${MAINLINE_BRANCH}" ]; then
+    # set the computed release name to the first 8 chars of the commit sha 
+    echo "$COMMITHASH:0:8}"
+
+elif [ "${REF}" =~ "${RELEASE_BRANCH_PATTERN}" ]; then 
+    echo "${BASH_REMATCH[1]}"
+
+else
+    echo "${REF}"
+
+fi
