60ef46e91212c53dbb98bb30630781e9306507c2
Zeno Jiricek <airtonix@users.noreply.github.com>
2022-12-25 11:18:22 +1030

    fix hash shortener

--- scripts/compute-release-name.sh
+++ scripts/compute-release-name.sh
@@ -8,7 +8,7 @@ COMMITHASH="${2}"
 
 if [ "${REF}" -eq "${MAINLINE_BRANCH}" ]; then
     # set the computed release name to the first 8 chars of the commit sha 
-    echo "$COMMITHASH:0:8}"
+    echo "${COMMITHASH:0:8}"
 
 elif [ "${REF}" =~ "${RELEASE_BRANCH_PATTERN}" ]; then 
     echo "${BASH_REMATCH[1]}"
